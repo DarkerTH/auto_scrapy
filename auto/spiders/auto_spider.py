6e9f31e39764dbdfaa7d02735ef2a7e0a325e4ea
Skirmantas Januškas <skirmantas.januskas@gmail.com>
2017-05-10 15:51:40 +0300

    Changed manufacturer/model input format

--- auto/spiders/auto_spider.py
+++ auto/spiders/auto_spider.py
@@ -12,7 +12,6 @@ from scrapy.crawler import CrawlerProcess
 
 import json
 
-file_name = sys.argv[1]
 items = []
 
 class AutoSpider(scrapy.Spider):
@@ -24,12 +23,12 @@ class AutoSpider(scrapy.Spider):
             {
               'title': 'autogidas',
               'ad-div': '.item-link',
-              'url': 'https://autogidas.lt/automobiliai/?f_1%5B1%5D={0}&f_model_14%5B1%5D={1}&f_41={2}&f_42={3}&f_215={4}&f_216={5}&f_60=6315&f_50=atnaujinimo_laika_asc'.format(self.manufacturer, self.model, self.year_from, self.year_to, self.price_from, self.price_to),
+              'url': 'https://autogidas.lt/automobiliai/?f_1%5B1%5D={0}&f_model_14%5B1%5D={1}&f_41={2}&f_42={3}&f_215={4}&f_216={5}&f_60=6315&f_50=atnaujinimo_laika_asc'.format(self.manufacturer_model['autogidas']['manufacturer'], self.manufacturer_model['autogidas']['model'], self.year_from, self.year_to, self.price_from, self.price_to),
             },
             {
               'title': 'autoplius',
               'ad-div': 'div.item',
-              'url': 'https://autoplius.lt/skelbimai/naudoti-automobiliai/{0}/{1}?engine_capacity_from=&engine_capacity_to=&power_from=&power_to=&kilometrage_from=&kilometrage_to=&has_damaged_id=&color_id=&condition_type_id=&make_date_from={2}&make_date_to={3}&sell_price_from={4}&sell_price_to={5}&fuel_id=&gearbox_id=&body_type_id=&wheel_drive_id=&number_of_seats_id=&number_of_doors_id=&fk_place_countries_id=&steering_wheel_id=&origin_country_id=&older_not=-1&qt=&order_by=3&order_direction=DESC'.format(self.manufacturer, self.model, self.year_from, self.year_to, self.price_from, self.price_to),
+              'url': 'https://autoplius.lt/skelbimai/naudoti-automobiliai/{0}/{1}?engine_capacity_from=&engine_capacity_to=&power_from=&power_to=&kilometrage_from=&kilometrage_to=&has_damaged_id=&color_id=&condition_type_id=&make_date_from={2}&make_date_to={3}&sell_price_from={4}&sell_price_to={5}&fuel_id=&gearbox_id=&body_type_id=&wheel_drive_id=&number_of_seats_id=&number_of_doors_id=&fk_place_countries_id=&steering_wheel_id=&origin_country_id=&older_not=-1&qt=&order_by=3&order_direction=DESC'.format(self.manufacturer_model['autoplius']['manufacturer'], self.manufacturer_model['autoplius']['model'], self.year_from, self.year_to, self.price_from, self.price_to),
             }
         ]
 
@@ -70,7 +69,7 @@ class AutoSpider(scrapy.Spider):
                     'title': item.css('.item-section .title-list a::text').extract_first(),
                     'year': item.css('.item-section .param-list div span[title="Pagaminimo data"]::text').extract_first(),
                     'fuel': item.css('.item-section .param-list div span[title="Kuro tipas"]::text').extract_first(),
-                    'gearbox': item.css('.item-section .param-list div span[title*="Pavar"]::text').extract()[0].encode('ascii'),
+                    'gearbox': item.css('.item-section .param-list div span[title*="Pavar"]::text').extract()[0],
                     'power': ''.join(item.css('.item-section .param-list div span[title="Galia"]::text').re(r'\d+')),
                     'mileage': ''.join(item.css('.item-section .param-list div span[title="Rida"]::text').re(r'\d+')),
                     'city': item.css('.item-section .param-list div span[title="Miestas"]::text').extract_first().encode('utf-8'),
@@ -94,9 +93,6 @@ def load_lines(path):
 
 process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-    'FEED_FORMAT': 'json',
-    'FEED_EXPORT_ENCODING': 'utf-8',
-    'FEED_URI': '{0}.json'.format(sys.argv[1]),
     'ROTATING_PROXY_LIST': load_lines('/var/www/html/auto_scrapy/auto/spiders/proxies.txt'),
     'DOWNLOADER_MIDDLEWARES': {
         'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': None,
@@ -105,5 +101,5 @@ process = CrawlerProcess({
     }
 })
 
-process.crawl(AutoSpider, manufacturer=sys.argv[2], model=sys.argv[3], year_from=sys.argv[4], year_to=sys.argv[5], price_from=sys.argv[6], price_to=sys.argv[7])
+process.crawl(AutoSpider, manufacturer_model=json.loads(sys.argv[1]), year_from=sys.argv[2], year_to=sys.argv[3], price_from=sys.argv[4], price_to=sys.argv[5])
 process.start() # the script will block here until the crawling is finished
